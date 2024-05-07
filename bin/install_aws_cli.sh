cac5680f57539d655e1c1aa5fe584c2701c2ad8e
Thiago Wingerter <thiagowingerter@hotmail.com>
2024-05-07 19:05:16 +0000

    #5 Add get-caller line for test

--- bin/install_aws_cli.sh
+++ bin/install_aws_cli.sh
@@ -7,4 +7,7 @@
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     unzip awscliv2.zip
     sudo ./aws/install
+
+    aws sts get-caller-identity
+
     cd $PROJECT_ROOT
