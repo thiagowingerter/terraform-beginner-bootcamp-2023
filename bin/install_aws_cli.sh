31f0464d6d2615bd3d1b9b6c5f60ebce165f4b9f
Thiago Wingerter <thiagowingerter@hotmail.com>
2024-05-07 19:02:13 +0000

    #5 Correct script error

--- bin/install_aws_cli.sh
+++ bin/install_aws_cli.sh
@@ -2,7 +2,7 @@
     cd /workspace
 
     rm -f '/workspace/awscliv2.zip'
-    rm -rf '/workspace/aws'\
+    rm -rf '/workspace/aws'
      
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     unzip awscliv2.zip
