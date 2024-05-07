2dc179181eced5c1b5cf1677440d1ed7a064ed30
Thiago Wingerter <thiagowingerter@hotmail.com>
2024-05-07 16:10:31 +0000

    provide example env for env var PROJECT_ROOT.

--- bin/install_terraform_cli.sh
+++ bin/install_terraform_cli.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+cd /workspace
+
 #Ensure that your system is up to date and you have installed the gnupg, software-properties-common, and curl packages installed.
 #You will use these packages to verify HashiCorp's GPG signature and install HashiCorp's Debian package repository.
 
@@ -31,3 +33,5 @@ sudo apt update
 #Install Terraform from the new repository.
 
 sudo apt-get install terraform
+
+cd $PROJECT_ROOT
