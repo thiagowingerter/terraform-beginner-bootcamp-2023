969ba1174dd8ac87035a9d364faab1f8e5bdadc3
Thiago_Wingerter <thiagowingerter@hotmail.com>
2024-05-08 15:44:46 -0300

    #9 install terraform provider, create s3 bucket (#10)

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/random"
       version = "3.6.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.48.0"
+    }
   }
 }
 
@@ -11,9 +15,24 @@ provider "random" {
   # Configuration options
 }
 
+provider "aws" {
+  # Configuration options
+}
+
 resource "random_string" "bucket_name" {
-  length           = 16
-  special          = false
+  length = 32
+  lower = true
+  upper = false
+  special = false
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
+
+#  tags = {
+ #   Name        = "My bucket"
+  #  Environment = "Dev"
+  #}
 }
 
 output "random_bucket_name" {
